7e7884cc7d80b93ec94b1f2962e3e3341cc37fb0
Jésus Brakmar <31973542+TERRUSS@users.noreply.github.com>
2018-02-15 17:35:35 +0000

    [wip] tryin fix multitracks

--- app.js
+++ app.js
@@ -1,113 +1,109 @@
 	/* APP.JS */
 
-const express = require('express');
-const http = require('http');
-const fs = require('fs');
+	const express = require('express');
+	const http = require('http');
+	const fs = require('fs');
 
-const app = express();
-const server = http.createServer(app);
-const io = require('socket.io')(server);
+	const app = express();
+	const server = http.createServer(app);
+	const io = require('socket.io')(server);
 
-const { exec } = require('child_process');
+	const { exec } = require('child_process');
 
-const { refreshList } = require('./tracks');
+	const { refreshList } = require('./tracks');
 
-app.use(express.static(__dirname + '/public'));
-
-
-  //routing
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+	app.use(express.static(__dirname + '/public'));
 
 
+	//routing
+	app.get('/', (req, res) => {
+	  res.sendFile(__dirname + '/index.html');
+	});
 
 
-var status = {
-  playing: null,
-  pause: true
-}
-
-
-
-
-  //and the interseting stuff begin...
-
-global.tracks = [];
-
-io.sockets.on('connection', (socket) => {
- 
-  if (tracks != ''){
-    socket.emit('trackList', tracks);
-  } else {
-   refreshList(function () {
-    	socket.emit('trackList', tracks);
-   })
-  }
-
-	
-	//refresh the tracklist
-  socket.on('reload', () => {
-	console.log('reload');
-
-	  tracks = refreshList("oi") 
-	setTimeout(()=>{
-	  socket.broadcast.emit('trackList', tracks);
-	  socket.emit('trackList', tracks);
- 	}, 1000);
-  });
-
-
-  //change song
-  socket.on("chSong", (path) => {
-	
-	if ( status.playing ) {
-		exec('killall play', (error, stdout, stderr) => {
-			if (error) {
-				console.log('ERROR : ' + error);
-			}
-			return;
-		});
+  //chhht, i'm handeling errors...
+	var status = {
+	  playing: null,
+	  pause: true
 	}
 
-     exec('play ' + '"' + path + '"', (error, stdout, stderr) => {
-       if (error) {
-         console.log("ERROR : " + error);
-    
-       	 socket.broadcast.emit('error', ('play', stderr));
-	       return;
-       }
-    
-      status.playing = path;
-      status.pause = false;
-      
-	     console.log("STDOUT : " + stdout);
+
+	//and the interseting stuff begin...
+
+	global.tracks = [];
+
+	io.sockets.on('connection', (socket) => {
+	  if (tracks != '') {
+	    socket.emit('trackList', tracks);
+	  } else {
+	    refreshList(function() {
+	      socket.emit('trackList', tracks);
+      })
+	  }
+
+
+	  //refresh the tracklist
+	  socket.on('reload', () => {
+	    console.log('reload');
+
+	    tracks = refreshList("oi")
+	    setTimeout(() => {
+	      socket.broadcast.emit('trackList', tracks);
+	      socket.emit('trackList', tracks);
+	    }, 1000);
+	  });
+
+
+	  //change song
+	  socket.on("chSong", (path) => {
+
+	    if (status.pause == false) {
+	      exec('killall play', (error, stdout, stderr) => {
+	        if (error) {
+	          console.log('ERROR : ' + error);
+	        }
+	        return;
+	      });
+	    }
+
+	    exec('play ' + '"' + path + '"', (error, stdout, stderr) => {
+	      if (error) {
+	        console.log("ERROR : " + error);
+
+	        socket.broadcast.emit('error', ('play', stderr));
+	        return;
+	      }
+
+	      status.playing = path;
+	      status.pause = false;
+
+	      console.log("STDOUT : " + stdout);
 	      console.log("STDERR : " + stderr);
 
-	    sudosocket.broadcast.emit('chSong', path);
-      		socket.emit('chSong', path);
-	socket.broadcast.emit('status', status);
-	socket.emit('status', status);
-    });
-  });
+	      sudosocket.broadcast.emit('chSong', path);
+	      socket.emit('chSong', path);
+	      socket.broadcast.emit('status', status);
+	      socket.emit('status', status);
+	    });
+	  });
 
-  //pause
+	  //pause
   socket.on('pause', () => {
-	if ( status.playing ) {
-		exec('killall play', (error, stdout, stderr) => {
-			if (error) {
-				console.log('ERROR : ' + error);
-			}
-		});
-	}
+    if (status.pause == false) {
+      exec('killall play', (error, stdout, stderr) => {
+        if (error) {
+          console.log('ERROR : ' + error);
+        }
+      });
+    }
 
     socket.broadcast.emit('pause', status.pause);
     socket.emit('pause', status.pause);
 
     status.pause = status.pause ? false : true; //for newies
 
-  });
+	  });
 
-});
+	});
 
-server.listen(80);
+	server.listen(80);
